2313ed0a50e338b5eba2d76fd009d3450540d53a
gabrielrodriguesleite <k.cogabriel@gmail.com>
2023-04-15 19:34:52 -0300

    chore: eslint config

--- algorithms.js
+++ algorithms.js
@@ -1,16 +1,16 @@
 const filter = (arr, type) => {
   switch (type) {
-    case "letters":
+    case 'letters':
       return arr.filter((i) => i.length == 1 && /[a-zA-Z]/.test(i))
 
-    case "numbers":
-      return arr.filter((i) => typeof i == "number")
+    case 'numbers':
+      return arr.filter((i) => typeof i == 'number')
 
-    case "highest":
-      return arr.filter((i) => typeof i == "number").reduce((p, c) => p < c ? c : p)
+    case 'highest':
+      return arr.filter((i) => typeof i == 'number').reduce((p, c) => p < c ? c : p)
 
     default:
-      return "Use \"letters\", \"numbers\" ou \"highest\" in the type argument."
+      return 'Use "letters", "numbers" ou "highest" in the type argument.'
   }
 };
 
@@ -19,15 +19,15 @@ const calc = {
   subtraction: (arr) => arr.reduce((p, c) => c - p, 0),
   multiplication: (arr) => arr.reduce((p, c) => c * p),
   division: (a, b) => {
-    if (b == 0) throw new Error("0 division error.")
+    if (b == 0) throw new Error('0 division error.')
     return a / b
   }
 }
 
-const newID = () => "XXXX-AAAA-BBBB-CCCC"
-  .split("")
-  .map((i) => i != "-" ? Math.random().toString(36)[3] : i)
-  .join("")
+const newID = () => 'XXXX-AAAA-BBBB-CCCC'
+  .split('')
+  .map((i) => i != '-' ? Math.random().toString(36)[3] : i)
+  .join('')
 
 
 module.exports = { filter, calc, newID }
--- algorithms.test.js
+++ algorithms.test.js
@@ -1,73 +1,75 @@
-const { filter, calc, newID } = require("./algorithms.js")
+const { filter, calc, newID } = require('./algorithms.js')
 
-const TEST_ARRAY = ["a", 10, "b", "hola", 122, 15]
-const TEST_ARRAY2 = ["bye", 324, "d", "X", 6534, "hello", 123, "e", "3", 765]
+const TEST_ARRAY = ['a', 10, 'b', 'hola', 122, 15]
+const TEST_ARRAY2 = ['bye', 324, 'd', 'X', 6534, 'hello', 123, 'e', '3', 765]
 
 const TEST_N_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const TEST_N_ARRAY2 = [324, 6534, 123, 765]
 
-describe("===== 1 - Teste da função filtro: =====", () => {
-  test("retorna um array de letras", () => {
-    expect(filter(TEST_ARRAY, "letters")).toEqual(["a", "b"])
-    expect(filter(TEST_ARRAY2, "letters")).toEqual(["d", "X", "e"])
+describe('===== 1 - Teste da função filtro: =====', () => {
+  test('retorna um array de letras', () => {
+    expect(filter(TEST_ARRAY, 'letters')).toEqual(['a', 'b'])
+    expect(filter(TEST_ARRAY2, 'letters')).toEqual(['d', 'X', 'e'])
   })
 
-  test("retorna um array de números:", () => {
-    expect(filter(TEST_ARRAY, "numbers")).toEqual([10, 122, 15])
-    expect(filter(TEST_ARRAY2, "numbers")).toEqual([324, 6534, 123, 765])
+  test('retorna um array de números:', () => {
+    expect(filter(TEST_ARRAY, 'numbers')).toEqual([10, 122, 15])
+    expect(filter(TEST_ARRAY2, 'numbers')).toEqual([324, 6534, 123, 765])
   })
 
-  test("retorna o maior número:", () => {
-    expect(filter(TEST_ARRAY, "highest")).toEqual(122)
-    expect(filter(TEST_ARRAY2, "highest")).toEqual(6534)
+  test('retorna o maior número:', () => {
+    expect(filter(TEST_ARRAY, 'highest')).toEqual(122)
+    expect(filter(TEST_ARRAY2, 'highest')).toEqual(6534)
   })
 
-  test("retorna uma string explicando o parametro \"type\" da função.", () => {
-    expect(filter(TEST_ARRAY, "")).toEqual("Use \"letters\", \"numbers\" ou \"highest\" in the type argument."
-    )
+  test('retorna uma string explicando o parametro "type" da função.', () => {
+    expect(filter(TEST_ARRAY, ''))
+      .toEqual('Use "letters", "numbers" ou "highest" in the type argument.'
+      )
   })
 })
 
-describe("===== 2 - Testes para calc: =====", () => {
-  test("\"addition\" soma os numeros do array:", () => {
+describe('===== 2 - Testes para calc: =====', () => {
+  test('"addition" soma os numeros do array:', () => {
     expect(calc.addition(TEST_N_ARRAY)).toEqual(45)
     expect(calc.addition(TEST_N_ARRAY2)).toEqual(7746)
   })
 
-  test("\"subtraction\" subtrai os numeros do array:", () => {
+  test('"subtraction" subtrai os numeros do array:', () => {
     expect(calc.subtraction(TEST_N_ARRAY)).toEqual(5)
     expect(calc.subtraction(TEST_N_ARRAY2)).toEqual(6852)
   })
 
-  test("\"multiplication\" multiplica os numeros do array:", () => {
+  test('"multiplication" multiplica os numeros do array:', () => {
     expect(calc.multiplication(TEST_N_ARRAY)).toEqual(362880)
     expect(calc.multiplication(TEST_N_ARRAY2)).toEqual(199200620520)
   })
 
-  test("\"division\" divide \"a\" por \"b\":", () => {
+  test('"division" divide "a" por "b":', () => {
     expect(calc.division(122, 2)).toEqual(61)
     expect(calc.division(6574, 2)).toEqual(3287)
   })
 
-  test("\"division\" lança um erro quando \"b\" é 0:", () => {
-    expect(() => calc.division(122, 0)).toThrow(new Error("0 division error."))
+  test('"division" lança um erro quando "b" é 0:', () => {
+    expect(() => calc.division(122, 0)).toThrow(new Error('0 division error.'))
   })
 })
 
-describe("===== 3 - Testes para newID: =====", () => {
-  const IDS = "1234567".split("").map(i => newID())
+describe('===== 3 - Testes para newID: =====', () => {
+  const IDS = '1234567'.split('').map(() => newID())
 
-  test.each(IDS)("a função deve retornar uma string contendo 4 grupos de 4 \n\tcaracteres alfanuméricos separados por traço \"-\".",
+  test.each(IDS)('a função deve retornar uma string contendo 4 grupos de '
+    + '4 \n\tcaracteres alfanuméricos separados por traço "-".',
     (i) => {
       expect(
         /[a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}/
-        .test(i)
+          .test(i)
       )
-      .toBeTruthy()
+        .toBeTruthy()
     })
 
-  test("a função deve retornar valores aleatórios.", () => {
-    for (i in IDS) {
+  test('a função deve retornar valores aleatórios.', () => {
+    for (let i in IDS) {
       if (i != IDS[0]) {
         expect(IDS[i]).not.toEqual(IDS[i - 1])
       }
